38bcaa8206b740eaee52fe4241c73ad51ce83b3a
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:37:49 +0100

    Update source.c

--- level8/source.c
+++ level8/source.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char *auth = NULL;
+char *service = NULL;
+
+int main(void) {
+  char buffer[128];
+
+  while (1) {
+    printf("%p, %p\n", auth, service);
+    
+    if (!fgets(buffer, 128, stdin)) {
+      return 0;
+    }
+
+    if (!strncmp(buffer, "auth ", 5)) {
+      auth = (char*)malloc(4);
+
+      if (strlen(buffer + 5) < 31) {
+        strcpy(auth, buffer + 5);
+      }
+    }
+
+    if (!strncmp(buffer, "reset", 5)) {
+      free(auth);
+    }
+
+    if (!strncmp(buffer, "service", 6)) {
+      service = strdup(buffer + 7);
+    }
+
+    if (!strncmp(buffer, "login", 5)) {
+      if (*auth + 32) {
+        system("/bin/sh");
+      } else {
+        fwrite("Password:\n", 1, 10, stdout);
+      }
+    }
+  }
+
+  return 0;
+}
