28e8e1bc8bce3b26215b4927b7f06b35555096d7
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:35:20 +0100

    Update source.c

--- level3/source.c
+++ level3/source.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int m;
+
+void v(void) {
+  char input[520];
+
+  fgets(input, 512, stdin);
+  printf(input);
+
+  if (m == 64) {
+    fwrite("Wait what?!\n", 1, 12, stdout);
+    system("/bin/sh");
+  }
+}
+
+int main(void) {
+  v();
+
+  return 0;
+}
