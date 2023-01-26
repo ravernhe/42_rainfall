7a600b6172b7a7ec9da7fd67c686d937a4fb754c
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:34:31 +0100

    Update source.c

--- level2/source.c
+++ level2/source.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void p(void) {
+  char input[76];
+  int memory_address;
+
+  fflush(stdout);
+  gets(input);
+
+  if ((memory_address & 0xb0000000) == 0xb0000000) {
+    printf("(%p)\n", &memory_address);
+    exit(1);
+  }
+
+  puts(input);
+  strdup(input);
+}
+
+int main(void) {
+  p();
+
+  return 0;
+}
