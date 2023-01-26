aac669105271e2cf02a131199a8e70f7733bfc82
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:36:52 +0100

    Update source.c

--- level6/source.c
+++ level6/source.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+typedef void(*function_pointer)(void);
+
+void n(void) {
+	system("/bin/cat /home/user/level7/.pass");
+}
+
+void m(void) {
+	puts("Nope");
+}
+
+int main(int argc, char **argv) {
+  char *input = (char*)malloc(64);
+  function_pointer *f = (function_pointer*)malloc(4);
+  *f = m;
+
+  strcpy(input, argv[1]);
+  (*f)();
+}
