639c1a9f5178664393510d325d6311593fb8456b
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:36:02 +0100

    Update source.c

--- level4/source.c
+++ level4/source.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int m;
+
+void p(char *string) {
+  printf(string);
+}
+
+void n(void) {
+  char input[520];
+
+  fgets(input, 512, stdin);
+  p(input);
+
+  if (m == 16930116) {
+    system("/bin/cat /home/user/level5/.pass");
+  }
+}
+
+int main(void) {
+  n();
+
+  return 0;
+}
