2e462e2ce142555a7cc3f2f150c65e68752076b9
ravernhe <45392691+ravernhe@users.noreply.github.com>
2023-01-26 14:36:25 +0100

    Update source.c

--- level5/source.c
+++ level5/source.c
@@ -1,1 +1,21 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+void o(void) {
+  system("/bin/sh");
+  _exit(1);
+}
+
+void n(void) {
+  char input[520];
+  
+  fgets(input, 512, stdin);
+  printf(input);
+  exit(1);
+}
+
+int main(void) {
+  n();
+  return 0;
+}
